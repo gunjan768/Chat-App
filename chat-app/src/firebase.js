ab12f4f34a191c9628c5011716bc8d3b026ca9c3
Gunjan <57636775+gunjan768@users.noreply.github.com>
2020-06-10 18:47:15 +0530

    Update firebase.js

--- chat-app/src/firebase.js
+++ chat-app/src/firebase.js
@@ -6,14 +6,14 @@ import 'firebase/analytics';
 
 var firebaseConfig = 
 {
-    apiKey: "AIzaSyDV99nE48sfdQIHYS7wTT3yp7s_-pIAaAM",
-    authDomain: "fir-chat-app-3d60b.firebaseapp.com",
-    databaseURL: "https://fir-chat-app-3d60b.firebaseio.com",
-    projectId: "fir-chat-app-3d60b",
-    storageBucket: "fir-chat-app-3d60b.appspot.com",
-    messagingSenderId: "938863791712",
-    appId: "1:938863791712:web:f76d9f861c93dc0fdefd05",
-    measurementId: "G-6C5XF8D0H4"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
